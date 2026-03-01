bb39822c6ff5cffd6573181726f0371f7ff8c93f
leafleafle <151811154+leafleafle@users.noreply.github.com>
2026-03-01 17:10:09 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+class Product{
+
+    int id;
+    String brand;
+    String model;
+
+    Product(int id, String brand, String model){
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+    }
+
+}
+
+class StockItem{
+
+    int id;
+    int productId;
+
+}
+
+public class Main{
+    public static void main(String[] args) {
+    
+    }
+}
